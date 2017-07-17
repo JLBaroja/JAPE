2babc25bf472ce2dd031d449993181e3584a6fd4
Laboratorio 25 <lab25@localhost.localdomain>
2017-07-16 22:03:46 -0500

    Daily data; cum_resp.csv

--- MoreCode/exploring_MED_output.R
+++ MoreCode/exploring_MED_output.R
@@ -124,116 +124,170 @@
 ## Exploring Concurrent Output
 # rm(list=ls())
 concurrent_data_folder <- '~/Documents/Luis/JAPE/ConcurrentData'
-concurrent_data_folder <- '~/Documents/Research/JAPE/ConcurrentData'
+# concurrent_data_folder <- '~/Documents/Research/JAPE/ConcurrentData'
 setwd(concurrent_data_folder)
 ct_full <- read.csv('concurrent_full.csv',stringsAsFactors = F)
 
-cumulative_stuff <- function(brd,sssn){
-  
-  # brd <- 'p004'
-  # sssn <- 's01'
-  
-  ld <- subset(ct_full,bird==brd&session==sssn)
-  
-  time <- seq(0,ceiling(tail(ld$session_time_sec,1)),1)
-  cum_reinf_left <- NA
-  cum_reinf_right <- NA
-  cum_resp_left <- NA
-  cum_resp_right <- NA
-  c_tt <- 0
-  for(tt in time){
-    c_tt <- c_tt+1
-    # Cum-Cum Responses
-    cum_resp_left[c_tt] <- sum(ld$event=='response_left_key'&ld$session_time_sec<=tt)
-    cum_resp_right[c_tt] <- sum(ld$event=='response_right_key'&ld$session_time_sec<=tt)
-    
-    # Cum-Cum Reinforcers
-    cum_reinf_left[c_tt] <- sum(ld$event=='feeder_on_left'&ld$session_time_sec<=tt)
-    cum_reinf_right[c_tt] <- sum(ld$event=='feeder_on_right'&ld$session_time_sec<=tt)
-  }
-  
-  return(list(time_definition=time,
-              cum_resp_left=cum_resp_left,
-              cum_resp_right=cum_resp_right,
-              cum_reinf_left=cum_reinf_left,
-              cum_reinf_right=cum_reinf_right))
-}
-
-
-
-
-
-
-
 birds <- sort(unique(ct_full$bird))
-sessions <- sort(unique(ct_full$session))
-
-brds_info <- vector('list',length(birds))
-# bird <- birds[1]
-for(bb in 1:length(birds)){
-  sssns_info <- vector('list',length(sessions))
-  # session <- sessions[1]
-  for(ss in 1:length(sessions)){
-    cs <- cumulative_stuff(birds[bb],sessions[ss])
-    sssns_info[[ss]] <- cs
-    print(c(birds[bb],sessions[ss]))
+bb <- birds[4]
+for(bb in birds){
+  ld <- subset(ct_full,bird==bb&event%in%c('session_start',
+                                           'reinforcer_scheduled_left',
+                                           'reinforcer_scheduled_right',
+                                           'feeder_on_left',
+                                           'response_left_key',
+                                           'feeder_on_right',
+                                           'response_right_key',
+                                           'session_end'))
+  print(c(bb,dim(ld)))
+  df=ld[order(ld$session),]
+  dresp=subset(df,event%in%c('response_left_key','response_right_key'))
+  for(ss in unique(dresp$session)){
+    sd <- subset(dresp,session==ss)
+    if(ss=='s01'){
+      cum_sec <- sd$session_time_sec
+    }
+    else{
+      cum_sec <- append(cum_sec,sd$session_time_sec+max(cum_sec))
+    }
   }
-  brds_info[[bb]] <- sssns_info
+  dresp$cum_sec=cum_sec
+  
+  cum_resp_left <- NULL
+  cum_resp_right <- NULL
+  cum_reinf_left <- NULL
+  cum_reinf_right <- NULL
+  c_tt <- 0
+  
+  for(tt in dresp$cum_sec){
+    c_tt <- c_tt+1
+    cum_resp_left[c_tt] <- nrow(dresp[dresp$cum_sec<=tt&dresp$event=='response_left_key',])
+    cum_resp_right[c_tt] <- nrow(dresp[dresp$cum_sec<=tt&dresp$event=='response_right_key',])
+    cum_reinf_left[c_tt] <- nrow(dresp[dresp$cum_sec<=tt&dresp$event=='feeder_on_left',])
+    cum_reinf_right[c_tt] <- nrow(dresp[dresp$cum_sec<=tt&dresp$event=='feeder_on_right',])
+    print(paste(round(tt/(60*60),3),'hours processed',collapse=' '))
+  }
 }
 
+dresp$cum_resp_left <- cum_resp_left
+dresp$cum_resp_right <- cum_resp_right
+dresp$cum_reinf_left <- cum_reinf_left
+dresp$cum_reinf_right <- cum_reinf_right
 
-dev.off()
-x11(width=20,height=7)
-layout(matrix(1:12,ncol=6))
-par(mar=rep(2,4),oma=rep(3,4),bg='#000000',fg='#aaaaaa',col.axis='#aaaaaa')
-colors <- c(heat.colors(length(sessions)-2),c('#00dd00','#00dddd'))
-for(bb in 1:length(birds)){
-  
-  # print(c(bb,
-  #         max(c(max(cum_reinf_left),max(cum_reinf_right))),
-  #         max(c(max(cum_resp_left),max(cum_resp_right)))))
-  
-  plot(NULL,ylim=c(0,4000),xlim=c(0,4000),type='l')
-  abline(0,1,lty='dashed')
-  abline(0,3,lty='solid',col='#888888')
-  mtext(birds[bb],3,cex=1.5,line=1)
-  for(ss in 1:length(sessions)){
-    sd <- brds_info[[bb]][[ss]]
-    lines(sd$cum_resp_right,sd$cum_resp_left,col=colors[ss])
-  }
-  # text(2500,500,paste('# Left:',max(cum_resp_left)),adj=c(1,-1))
-  # text(2500,500,paste('# Right:',max(cum_resp_right)),adj=c(1,.5))
-  # text(2500,500,paste('# Total:',max(cum_resp_left)+max(cum_resp_right)),adj=c(1,2))
-  
-  plot(NULL,ylim=c(0,160),xlim=c(0,160),type='l')
-  abline(0,1,lty='dashed')
-  abline(0,3,lty='solid',col='#888888')
-  for(ss in 1:length(sessions)){
-    sd <- brds_info[[bb]][[ss]]
-    lines(sd$cum_reinf_right,sd$cum_reinf_left,col=colors[ss])
-  }
-  
-  # text(150,25,paste('# Left:',max(cum_reinf_left)),adj=c(1,-1))
-  # text(150,25,paste('# Right:',max(cum_reinf_right)),adj=c(1,.5))
-  # text(150,25,paste('# Total:',max(cum_reinf_left)+max(cum_reinf_right)),adj=c(1,2))
-}
+write.csv(dresp,'cum_resp_coco.csv',row.names=F)
+tail(cum_resp_left)
 
+par(mar=rep(0,4))
+plot(cum_resp_left,cum_resp_right,xlim=c(0,25000),ylim=c(0,25000));abline(0,1,lty='dashed')
 
+# 
+# cumulative_stuff <- function(brd,sssn){
+#   
+#   # brd <- 'p004'
+#   # sssn <- 's01'
+#   
+#   ld <- subset(ct_full,bird==brd&session==sssn)
+#   
+#   time <- seq(0,ceiling(tail(ld$session_time_sec,1)),1)
+#   cum_reinf_left <- NA
+#   cum_reinf_right <- NA
+#   cum_resp_left <- NA
+#   cum_resp_right <- NA
+#   c_tt <- 0
+#   for(tt in time){
+#     c_tt <- c_tt+1
+#     # Cum-Cum Responses
+#     cum_resp_left[c_tt] <- sum(ld$event=='response_left_key'&ld$session_time_sec<=tt)
+#     cum_resp_right[c_tt] <- sum(ld$event=='response_right_key'&ld$session_time_sec<=tt)
+#     
+#     # Cum-Cum Reinforcers
+#     cum_reinf_left[c_tt] <- sum(ld$event=='feeder_on_left'&ld$session_time_sec<=tt)
+#     cum_reinf_right[c_tt] <- sum(ld$event=='feeder_on_right'&ld$session_time_sec<=tt)
+#   }
+#   
+#   return(list(time_definition=time,
+#               cum_resp_left=cum_resp_left,
+#               cum_resp_right=cum_resp_right,
+#               cum_reinf_left=cum_reinf_left,
+#               cum_reinf_right=cum_reinf_right))
+# }
+# 
+# 
+# 
+# 
 
+# 
+# 
+# birds <- sort(unique(ct_full$bird))
+# sessions <- sort(unique(ct_full$session))
+# 
+# brds_info <- vector('list',length(birds))
+# # bird <- birds[1]
+# for(bb in 1:length(birds)){
+#   sssns_info <- vector('list',length(sessions))
+#   # session <- sessions[1]
+#   for(ss in 1:length(sessions)){
+#     cs <- cumulative_stuff(birds[bb],sessions[ss])
+#     sssns_info[[ss]] <- cs
+#     print(c(birds[bb],sessions[ss]))
+#   }
+#   brds_info[[bb]] <- sssns_info
+# }
 
+# 
+# dev.off()
+# x11(width=20,height=7)
+# layout(matrix(1:12,ncol=6))
+# par(mar=rep(2,4),oma=rep(3,4),bg='#000000',fg='#aaaaaa',col.axis='#aaaaaa')
+# colors <- c(heat.colors(length(sessions)-2),c('#00dd00','#00dddd'))
+# for(bb in 1:length(birds)){
+#   
+#   # print(c(bb,
+#   #         max(c(max(cum_reinf_left),max(cum_reinf_right))),
+#   #         max(c(max(cum_resp_left),max(cum_resp_right)))))
+#   
+#   # plot(NULL,ylim=c(0,4000),xlim=c(0,4000),type='l')
+#   plot(NULL,ylim=c(0,1000),xlim=c(0,1000),type='l')
+#   abline(0,1,lty='dashed')
+#   abline(0,3,lty='solid',col='#888888')
+#   mtext(birds[bb],3,cex=1.5,line=1)
+#   for(ss in 1:length(sessions)){
+#     sd <- brds_info[[bb]][[ss]]
+#     lines(sd$cum_resp_right,sd$cum_resp_left,col=colors[ss])
+#   }
+#   # text(2500,500,paste('# Left:',max(cum_resp_left)),adj=c(1,-1))
+#   # text(2500,500,paste('# Right:',max(cum_resp_right)),adj=c(1,.5))
+#   # text(2500,500,paste('# Total:',max(cum_resp_left)+max(cum_resp_right)),adj=c(1,2))
+#   
+#   plot(NULL,ylim=c(0,160),xlim=c(0,160),type='l')
+#   abline(0,1,lty='dashed')
+#   abline(0,3,lty='solid',col='#888888')
+#   for(ss in 1:length(sessions)){
+#     sd <- brds_info[[bb]][[ss]]
+#     lines(sd$cum_reinf_right,sd$cum_reinf_left,col=colors[ss])
+#   }
+#   
+#   # text(150,25,paste('# Left:',max(cum_reinf_left)),adj=c(1,-1))
+#   # text(150,25,paste('# Right:',max(cum_reinf_right)),adj=c(1,.5))
+#   # text(150,25,paste('# Total:',max(cum_reinf_left)+max(cum_reinf_right)),adj=c(1,2))
+# }
+# 
+# 
 
-
-bb <- birds[3]
-b_data <- subset(ct_full,bird==bb)
-# ss <- sessions[1]
-dev.off()
-x11(width=20,height=10)
-layout(1:length(sessions))
-par(mar=rep(2,4),oma=c(0,0,3,0))
-for(ss in sessions){
-  s_data <- subset(b_data,session==ss)
-  plot_session(s_data)
-}
-mtext(bb,3,outer=T,cex=1.5)
-
-
+# 
+# 
+# 
+# bb <- birds[3]
+# b_data <- subset(ct_full,bird==bb)
+# # ss <- sessions[1]
+# dev.off()
+# x11(width=20,height=10)
+# layout(1:length(sessions))
+# par(mar=rep(2,4),oma=c(0,0,3,0))
+# for(ss in sessions){
+#   s_data <- subset(b_data,session==ss)
+#   plot_session(s_data)
+# }
+# mtext(bb,3,outer=T,cex=1.5)
+# 
+# 
--- MoreCode/exploring_MED_output.py
+++ MoreCode/exploring_MED_output.py
@@ -5,7 +5,7 @@ fd=pd.read_csv('ConcurrentData/concurrent_full.csv')
 
 # Pick bird and session
 bb='p510'
-ss='s16'
+ss='s10'
 
 ld=fd.loc[(fd.bird==bb)&(fd.session==ss)]
 
@@ -13,15 +13,44 @@ rsp_left=ld.loc[ld.event=='response_left_key'].session_time_sec
 rsp_right=ld.loc[ld.event=='response_right_key'].session_time_sec
 rnf_left=ld.loc[ld.event=='feeder_on_left'].session_time_sec
 rnf_right=ld.loc[ld.event=='feeder_on_right'].session_time_sec
-
-lever=all_rsp=pd.concat([pd.Series(np.repeat('left',len(rsp_left))),
+all_rsp=pd.concat([rsp_left,rsp_right])
+lever=pd.concat([pd.Series(np.repeat('left',len(rsp_left))),
 	pd.Series(np.repeat('right',len(rsp_right)))])
+all_rsp.index=lever.index
+resp_dict={'lever':lever,'session_time':all_rsp}
+
+responses=pd.DataFrame(data=resp_dict,index=all_rsp.index)
+responses=responses.sort_values('session_time')
+responses.index=range(len(responses))
+
+responses['leave']=False
+responses['engage']=True
+responses['bout']=False
+
+leave=responses.leave.values
+engage=responses.engage.values
+bout=responses.bout.values
+key=responses.lever.values
+tm=responses.session_time
+
+ii=0
+while ii < (len(key)-1):
+	if key[ii+1]!=key[ii]:
+		leave[ii]=True
+	if key[ii+1]==key[ii]:
+		engage[ii+1]=False
+
+	ii=ii+1
+
+bout[(leave==False)&(engage==False)]=True
+responses['reinforced']=(tm.isin(rnf_left)|tm.isin(rnf_right))
 
 
-all_resp=pd.concat([rsp_left,rsp_right],ignore_index=True)
-all_reinf=pd.concat([rnf_left,rnf_right],ignore_index=True)
-all_resp.sort_values(inplace=True)
-all_reinf.sort_values(inplace=True)
+
+#all_resp=pd.concat([rsp_left,rsp_right],ignore_index=True)
+#all_reinf=pd.concat([rnf_left,rnf_right],ignore_index=True)
+#all_resp.sort_values(inplace=True)
+#all_reinf.sort_values(inplace=True)
 
 # Next: generate data frames at RESPONSE, VISIT, and REINFORCER levels
 
