ce8a2edbc1914cf95ab5d7d867ffd669ad3c8452
jlbm <j.luis.baroja@gmail.com>
2017-08-25 17:33:34 -0500

    Session 60

--- MoreCode/new_cum_records.R
+++ MoreCode/new_cum_records.R
@@ -1,5 +1,6 @@
 rm(list=ls())
-setwd('~/Documents/Luis/JAPE/ConcurrentData/CSV files/')
+# setwd('~/Documents/Luis/JAPE/ConcurrentData/CSV files/')
+setwd('~/Documents/Research/JAPE/ConcurrentData/CSV files/')
 
 birds <- c('p004','p054','p138','p510','p530','p736')
 # sessions <- 1:70
@@ -10,14 +11,19 @@ time_definition <- 1
 data_file <- paste(bb,'s',sprintf('%02d',ss),'.csv',sep='')
 if(data_file%in%dir()){
   df <- read.csv(data_file)}
-times <- seq(0,max(df$session_time_sec),by=time_definition)
+time <- seq(0,max(df$session_time_sec),by=time_definition)
 cum_resp_right <- NULL
 cum_resp_left <- NULL
-for(tt in times){
-  cum_resp_right <- append(cum_resp_right,
-                           sum(df$session_time_sec<=tt&df$event=='response_right_key'))
-  cum_resp_left <- append(cum_resp_left,
-                          sum(df$session_time_sec<=tt&df$event=='response_left_key'))
+cum_reinf_right <- NULL
+cum_reinf_left <- NULL
+for(tt in time){
+  cum_resp_right <- append(cum_resp_right,sum(df$session_time_sec<=tt&df$event=='response_right_key'))
+  cum_resp_left <- append(cum_resp_left,sum(df$session_time_sec<=tt&df$event=='response_left_key'))
+  cum_reinf_right <- append(cum_reinf_right,sum(df$session_time_sec<=tt&df$event=='feeder_on_right'))
+  cum_reinf_left <- append(cum_reinf_left,sum(df$session_time_sec<=tt&df$event=='feeder_on_left'))
 }
-data.frame(times,cum_resp_left,cum_resp_right)
-
+data.frame(time,cum_resp_left,cum_resp_right,cum_reinf_left,cum_reinf_right)
+plot(cum_resp_right,cum_resp_left,
+     type='l',
+     xlim=c(0,max(c(max(cum_resp_left),max(cum_resp_right)))),
+     ylim=c(0,max(c(max(cum_resp_left),max(cum_resp_right)))))
